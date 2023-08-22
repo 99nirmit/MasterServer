7d889b80277cf53b6e827be08bc0656ba66406ed
Uday Shastrakar <uday.shastrakar@gmail.com>
2023-08-22 23:40:14 +0530

    added configs of api gateway

--- apigateway/src/main/kotlin/com/example/apigateway/config/ApiGatewayConfig.kt
+++ apigateway/src/main/kotlin/com/example/apigateway/config/ApiGatewayConfig.kt
@@ -9,34 +9,34 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ApiGatewayConfig {
-    @Bean
-    fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator? {
-        return builder.routes()
-            .route(
-                "example_route"
-            ) { r: PredicateSpec ->
-                r
-                    .path("/example/**")
-                    .uri("http://example.com")
-            }
-            .route(
-                "another_route"
-            ) { r: PredicateSpec ->
-                r
-                    .path("/another/**")
-                    .uri("http://another-service.com")
-            } // Add more routes as needed
-            .build()
-    }
+//    @Bean
+//    fun customRouteLocator(builder: RouteLocatorBuilder): RouteLocator? {
+//        return builder.routes()
+//            .route(
+//                "example_route"
+//            ) { r: PredicateSpec ->
+//                r
+//                    .path("/example/**")
+//                    .uri("http://example.com")
+//            }
+//            .route(
+//                "another_route"
+//            ) { r: PredicateSpec ->
+//                r
+//                    .path("/another/**")
+//                    .uri("http://another-service.com")
+//            } // Add more routes as needed
+//            .build()
+//    }
 
-    @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityWebFilterChain? {
-        http
-            .authorizeExchange()
-            .pathMatchers("/example/**").authenticated()
-            .anyExchange().permitAll()
-            .and()
-            .httpBasic()
-        return http.build()
-    }
+//    @Bean
+//    fun securityFilterChain(http: HttpSecurity): SecurityWebFilterChain? {
+//        http
+//            .authorizeExchange()
+//            .pathMatchers("/example/**").authenticated()
+//            .anyExchange().permitAll()
+//            .and()
+//            .httpBasic()
+//        return http.build()
+//    }
 }
